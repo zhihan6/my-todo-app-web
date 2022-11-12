79a988111e92a72779d86429a31d126e6e8254ca
zhihan <zhihan6@stanford.edu>
2022-11-12 12:16:51 -0800

    Without "complete" version

--- web.py
+++ web.py
@@ -17,11 +17,7 @@ st.write("This app is to increase your productivity")
 # Show todos list
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.experimental_rerun()
+    
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
